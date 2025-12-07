ab44192002c54b5a8bbe740a15c8f9798b792a3a
Захар Реутов <reutovz809@gmail.com>
2025-12-07 22:42:18 +0500

    Add project files.

---
+++ Tyuiu.ReutovZA.Sprint3.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ReutovZA.Sprint3.Task0.V22.Lib;
+
+namespace Tyuiu.ReutovZA.Sprint3.Task0.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        DataService ds = new DataService();
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 8;
+            var res = ds.GetMultiplySeries(value, startValue, stopValue);
+            Assert.AreEqual(0, res);
+        }
+    }
+}
---
+++ Tyuiu.ReutovZA.Sprint3.Task0.V22/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.ReutovZA.Sprint3.Task0.V22.Lib;
+
+namespace Tyuiu.ReutovZA.Sprint3.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Реутов З.А| ПКТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Выполнил: Реутов З.А| ПКТб-25-1                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение   *");
+            Console.WriteLine("* ряда по формуле, при a=0,25                                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 8;
+            Console.WriteLine("Переменная a = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var res = ds.GetMultiplySeries(value, startValue, stopValue);
+            Console.WriteLine("Произведение ряда = " + res);
+            Console.ReadKey();
+        }
+    }
+}
